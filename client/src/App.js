5421b76315195d3b327b5c9519aabbadf9b0b1a7
SidM77 <siddanth.manoj@gmail.com>
2024-03-18 16:45:12 +0530

    Added chat route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Complaint from "./Pages/Complaint";
 import Home from "./Pages/Home";
 import Repair from "./Pages/Repair";
 import Query from "./Pages/Query";
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/*<Route path="/query" element={<Query />} />*/}
+        {/*<Route path="/complaint" element={<Complaint />} />*/}
         <Route path="/query" element={<TempChat />} />
         <Route path="/repair" element={<Repair />} />
       </Routes>
